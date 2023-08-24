0b3355df477d2bbe7a634412f32e028274a9f87b
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-24 19:50:52 +0200

    changed links with correct route

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -11,11 +11,11 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
                 </li>
             </ul>
             <a class="nav-link active" aria-current="page" href="#">Login</a>
-            <a href="login.html" class="btn ms-4">Create an account</a>
+            <a href="#" class="btn ms-4">Create an account</a>
         </div>
     </div>
 </header>
