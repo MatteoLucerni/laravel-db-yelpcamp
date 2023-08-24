c2ebe007f80f7bd85a498fe660f0565a9b0840ac
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-24 18:43:09 +0200

    added route for index of camps

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/camp-list', function () {
+    return view('camps.index');
+})->name('index');
